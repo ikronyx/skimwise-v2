cc9e261f4b130c04c7182bfc617cebfe68193ac8
iKronyx <ikronyx@gmail.com>
2025-08-07 14:30:29 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -19,8 +19,8 @@ app = FastAPI()
 
 # Allow only your frontend domain
 origins = [
-    "skimwise-v2.vercel.app",
-    "skimwise-v2-bdl6.vercel.app",
+    "https://skimwise-v2.vercel.app",
+    "https://skimwise-v2-bdl6.vercel.app",
     "https://skimwise-v2-d3ol15wuf-ikronyxs-projects.vercel.app"
 ]
 
